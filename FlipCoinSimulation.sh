9a0e241f8cd993bbfb9b828bc34f76cc10dee45f
PratapPawar <pratappawar1997@gmail.com>
2021-03-31 10:00:49 +0530

    Added: Showing Number of time head or tail won

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -9,3 +9,21 @@ if [ $result -eq $Head ]
    else
 	echo "Tail is Winner"
 fi
+
+#As a simulator, loop through Multiple times of flipping a coin and show number of times head and tail has won
+
+#variable
+HeadCount=0
+TailCount=0
+read -p "Enter the flip in Number" NoOfCoinFlip
+for (( count=1; count<=NoOfCoinFlip; count++ ))
+     do
+	if [ $((RANDOM%2)) -eq $Head ]
+	   then
+		((HeadCount++))
+	else
+	       ((TailCount++))
+	fi
+     done
+echo  "Head Count=" $HeadCount
+echo  "Tail Count=" $TailCount
