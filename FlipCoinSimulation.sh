793756427f73bf6e841622c5862cd388680348cc
PratapPawar <pratappawar1997@gmail.com>
2021-03-31 08:56:56 +0530

    Added: To Display Head or Tail Who is Winner

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,2 +1,11 @@
 #! /bin/bash
 echo "Welcome to flip coin Simulator"
+#To Print Head and Tail 
+Head=1
+result=$((RANDOM%2))
+if [ $result -eq $Head ]
+   then
+	echo "Head is Winner"
+   else
+	echo "Tail is Winner"
+fi
